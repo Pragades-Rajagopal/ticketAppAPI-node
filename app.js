b00ef07caf9f683dfbb32c291105fed0a8b73ce8
Pragades-Rajagopal <pragadesgoldsmith@outlook.com>
2022-02-21 00:40:05 +0530

    added extra endpoints

--- app.js
+++ app.js
@@ -35,6 +35,45 @@ app.get('/ticketapp-api/:month', (req, res) => {
     res.status(200).json(data);
 });
 
+app.get('/ticketapp-api/resolution/:value', (req, res) => {
+    var resolution_ = req.params.value;
+    resolution_ = resolution_.split('+').join(' ');
+
+    const data = model.getResolution(resolution_);
+
+    if (data.length === 0) {
+        res.status(404).json({"no data":"no output for provided resolution"});
+        return;
+    }
+
+    return res.status(200).json(data);
+});
+
+app.get('/ticketapp-api/all/resolutions', (req, res) => {
+    const data = model.getAllResolutions();
+    var length = data.length;
+
+    var param_resol = [];
+    for (let i = 0; i < length; i++) {
+        let value = []
+        value[i] = data[i]["RESOLUTION"];
+        value[i] = value[i].split(' ').join('+');
+        param_resol.push(value[i]);
+    }    
+
+    var resol_ = [];
+    for (let i = 0; i < length; i++) {
+        resol_.push(data[i]["RESOLUTION"]);
+    }
+
+    var result = [];
+    for (let i = 0; i < length; i++) {
+        result.push({"RESOLUTION":resol_[i], "PARAM_RESULT":param_resol[i]});
+    }
+    
+    res.status(200).json(result);
+});
+
 app.listen(
     port
     // , () => console.log(`Server is running in port ${port}`)
--- models/appModels.js
+++ models/appModels.js
@@ -18,8 +18,23 @@ const getMonths = () => {
     return db.appDatabase.prepare(sql).all();
 };
 
+const getResolution = (resolution) => {
+    const sql = "SELECT MONTH, TICKET_COUNT from TICKETS_V2 tv where RESOLUTION = ?"
+
+    return db.appDatabase.prepare(sql).all(resolution);
+};
+
+const getAllResolutions = () => {
+    const sql = "SELECT DISTINCT RESOLUTION from TICKETS_V2";
+
+    return db.appDatabase.prepare(sql).all();
+};
+
+
 module.exports = {
     getData,
     getDataByMonth,
-    getMonths
+    getMonths,
+    getResolution,
+    getAllResolutions
 }
